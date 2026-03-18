a5bdde611f6a22be00aa897ececc0d132b56aa5b
zoaavela <enzooabdi@gmail.com>
2026-03-18 15:03:27 +0100

    CORS gestion

--- vue/src/services/adminService.js
+++ vue/src/services/adminService.js
@@ -4,6 +4,7 @@ export const importCsv = (file) => {
     const formData = new FormData();
     formData.append('file', file);
 
+    // Le token est automatiquement ajouté par api.js
     return apiFetch('/admin/import', {
         method: 'POST',
         body: formData
