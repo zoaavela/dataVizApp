ed885878a32147578d90f3dfa15ffe8b4fefa99c
zoaavela <enzooabdi@gmail.com>
2026-03-18 14:44:25 +0100

    add c hand background

---
+++ vue/src/components/Hand/Hand.jsx
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function Hand() {
+    return (
+        <main>
+            <Spline
+                scene="https://prod.spline.design/Hx4YiNij3PQvCW59/scene.splinecode"
+            />
+        </main>
+    );
+}
