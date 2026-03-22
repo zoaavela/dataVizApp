9ad65b411858763ca4dc9e7d465f067a1ad9c851
zoaavela <enzooabdi@gmail.com>
2026-03-22 16:15:36 +0100

    transitions

--- vue/src/components/PageTransition/PageTransition.jsx
+++ vue/src/components/PageTransition/PageTransition.jsx
@@ -1,20 +1,12 @@
 import { motion } from 'framer-motion';
 
-const animations = {
-    initial: { opacity: 0, y: 10 },
-    animate: { opacity: 1, y: 0 },
-    exit: { opacity: 0, y: -10 },
-};
-
 export default function PageTransition({ children }) {
     return (
         <motion.div
-            variants={animations}
-            initial="initial"
-            animate="animate"
-            exit="exit"
-            transition={{ duration: 0.4, ease: "easeInOut" }}
-            style={{ width: '100%', height: '100%' }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3 }}
         >
             {children}
         </motion.div>
