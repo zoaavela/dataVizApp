33e1fcd0c55f80a72393bc74f42e3648f3ac9e7e
zoaavela <enzooabdi@gmail.com>
2026-03-22 18:05:37 +0100

    local API

--- vue/src/services/authService.js
+++ vue/src/services/authService.js
@@ -1,7 +1,7 @@
 import { apiFetch } from './api';
 
 export const login = async (email, password) => {
-    const response = await fetch('https://zoaavela.alwaysdata.net/api/login', {
+    const response = await fetch('http://localhost:8000/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password })
